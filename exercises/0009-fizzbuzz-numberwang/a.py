cceca360233a5742a0846b309b5c2a450b54f7ce
subbuvincent <subbuvincent@stanford.edu>
2016-01-21 13:45:40 -0800

    Comm 213 Thurs 1-20 assignment

---
+++ exercises/0009-fizzbuzz-numberwang/a.py
@@ -0,0 +1,15 @@
+import requests
+
+for x in range (1,31):
+	
+
+	if x % 2 is 0:
+		if x % 7 is 0: 
+			print("Numberwang!")
+		else:
+			print("Numbe")
+	else: 
+		if x % 7 is 0:
+			print("rwang")
+		else:
+			print(x)
